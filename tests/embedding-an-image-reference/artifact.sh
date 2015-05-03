64b89a35d039fdc1346caed35a7323171791d2db
Tim Lucas <t@toolmantim.com>
2015-05-04 00:35:21 +1000

    Simpify the artifact image test

--- tests/embedding-an-image-reference/artifact.sh
+++ tests/embedding-an-image-reference/artifact.sh
@@ -2,8 +2,13 @@
 
 BASEDIR=$(dirname $0)
 
+function print_image_ref {
+    printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
+}
+
 cp $BASEDIR/../../fixtures/meme.png .
 cp $BASEDIR/../../fixtures/omg.gif .
 
-. $BASEDIR/../../utils/imgref.sh "artifact://meme.png" "Meme!"
-. $BASEDIR/../../utils/imgref.sh "artifact://omg.gif" "OMG!"
+print_image_ref "artifact://meme.png" "Meme!"
+
+print_image_ref "artifact://omg.gif" "OMG!"
