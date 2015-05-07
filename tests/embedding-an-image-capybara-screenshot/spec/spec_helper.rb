0b7b6432722b69bf5dd5df078872b6e6b5d57c7b
Tim Lucas <t@toolmantim.com>
2015-05-07 18:36:19 +1000

    Switch to capybara-inline-screenshot

--- tests/embedding-an-image-capybara-screenshot/spec/spec_helper.rb
+++ tests/embedding-an-image-capybara-screenshot/spec/spec_helper.rb
@@ -1,38 +1,12 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'capybara-screenshot'
-require 'capybara-screenshot/rspec'
+require 'capybara-inline-screenshot/rspec'
 
 Capybara.configure do |config|
   config.default_driver = :poltergeist
 end
 
-SCREENSHOT_PATH = "tmp/screenshots"
+CapybaraInlineScreenshot.base_artifact_dir = File.expand_path(File.join(__FILE__, "../../"))
 
-Capybara.save_and_open_page_path = File.expand_path(File.join(__FILE__, "../../", SCREENSHOT_PATH))
-
-module BuildkiteArtifactScreenshotReporter
-  require "base64"
-
-  extend Capybara::Screenshot::RSpec::BaseReporter
-
-  enhance_with_screenshot :example_failed
-
-  def example_failed_with_screenshot(notification)
-    example_failed_without_screenshot(notification)
-
-    return unless (screenshot = notification.example.metadata[:screenshot])
-
-    name = File.basename(screenshot[:image])
-
-    if ENV['BUILDKITE']
-      output.puts "\e]1338;url='artifact://#{File.join(SCREENSHOT_PATH, name)}';alt='#{name}'\a"
-    else
-      image = Base64.encode64(File.read(screenshot[:image])).gsub(/\n/, '')
-      output.puts "\e]1337;File=#{name}:#{image}\a"
-    end
-  end
-end
-
-Capybara::Screenshot::RSpec::REPORTERS["RSpec::Core::Formatters::ProgressFormatter"] = BuildkiteArtifactScreenshotReporter
-Capybara::Screenshot::RSpec::REPORTERS["RSpec::Core::Formatters::DocumentationFormatter"] = BuildkiteArtifactScreenshotReporter
+Capybara.save_and_open_page_path = File.join(CapybaraInlineScreenshot.base_artifact_dir, "tmp/screenshots")
