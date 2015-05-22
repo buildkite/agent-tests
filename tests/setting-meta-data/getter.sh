f03c37dead14375186a66cfbbfc7bb6094c27115
Keith Pitt <me@keithpitt.com>
2015-05-22 15:44:31 +0200

    Tweaked the getter script.

--- tests/setting-meta-data/getter.sh
+++ tests/setting-meta-data/getter.sh
@@ -5,6 +5,7 @@ if [ -z "$KEY_TO_GET" ]; then
   KEY_TO_GET="foo"
 fi
 
-echo "Key: $KEY_TO_GET"
+VALUE=$(buildkite-agent meta-data get $KEY_TO_GET)
 
-echo `buildkite-agent meta-data get $KEY_TO_GET`
+echo "Key: $KEY_TO_GET"
+echo "Value returned: $VALUE"
