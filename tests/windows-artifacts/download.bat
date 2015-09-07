048c1d64f74bc7bf9493ff35126548f475ccfc00
Keith Pitt <me@keithpitt.com>
2015-09-07 12:21:07 -0700

    Expanded windows artifact download formats

--- tests/windows-artifacts/download.bat
+++ tests/windows-artifacts/download.bat
@@ -1,14 +1,29 @@
-ECHO --- *nix slashes
-
+ECHO --- Downloading fixtures/embedded/folder/AlfredHitchcock.jpg
 call buildkite-agent artifact download "fixtures/embedded/folder/AlfredHitchcock.jpg" "."
 
-ECHO --- Windows slashes
+ECHO --- Downloading fixtures/embedded/folder/*
+call buildkite-agent artifact download "fixtures/embedded/folder/*" "."
 
+ECHO --- Downloading fixtures/embedded/**/*
+call buildkite-agent artifact download "fixtures/embedded/**/*" "."
+
+ECHO --- Downloading fixtures/**/*
+call buildkite-agent artifact download "fixtures/**/*" "."
+
+ECHO --- Downloading **/*
+call buildkite-agent artifact download "**/*" "."
+
+ECHO --- Downloading fixtures\embedded\folder\AlfredHitchcock.jpg
 call buildkite-agent artifact download "fixtures\embedded\folder\AlfredHitchcock.jpg" "."
 
-ECHO --- With wildcards
-
+ECHO --- Downloading fixtures\embedded\folder\*
 call buildkite-agent artifact download "fixtures\embedded\folder\*" "."
+
+ECHO --- Downloading fixtures\embedded\**\*
 call buildkite-agent artifact download "fixtures\embedded\**\*" "."
+
+ECHO --- Downloading fixtures\**\*
 call buildkite-agent artifact download "fixtures\**\*" "."
+
+ECHO --- Downloading **\*
 call buildkite-agent artifact download "**\*" "."
