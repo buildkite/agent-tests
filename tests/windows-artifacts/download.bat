b6c6825aa557cb52993624227de824272280a5e0
Keith Pitt <me@keithpitt.com>
2015-09-07 12:40:25 -0700

    Download files to seperate folders for post-run testing

--- tests/windows-artifacts/download.bat
+++ tests/windows-artifacts/download.bat
@@ -1,29 +1,29 @@
 ECHO --- Downloading fixtures/embedded/folder/AlfredHitchcock.jpg
-call buildkite-agent artifact download "fixtures/embedded/folder/AlfredHitchcock.jpg" "."
+call buildkite-agent artifact download "fixtures/embedded/folder/AlfredHitchcock.jpg" "d1"
 
 ECHO --- Downloading fixtures/embedded/folder/*
-call buildkite-agent artifact download "fixtures/embedded/folder/*" "."
+call buildkite-agent artifact download "fixtures/embedded/folder/*" "d2"
 
 ECHO --- Downloading fixtures/embedded/**/*
-call buildkite-agent artifact download "fixtures/embedded/**/*" "."
+call buildkite-agent artifact download "fixtures/embedded/**/*" "d3"
 
 ECHO --- Downloading fixtures/**/*
-call buildkite-agent artifact download "fixtures/**/*" "."
+call buildkite-agent artifact download "fixtures/**/*" "d4"
 
 ECHO --- Downloading **/*
-call buildkite-agent artifact download "**/*" "."
+call buildkite-agent artifact download "**/*" "d5"
 
 ECHO --- Downloading fixtures\embedded\folder\AlfredHitchcock.jpg
-call buildkite-agent artifact download "fixtures\embedded\folder\AlfredHitchcock.jpg" "."
+call buildkite-agent artifact download "fixtures\embedded\folder\AlfredHitchcock.jpg" "d6"
 
 ECHO --- Downloading fixtures\embedded\folder\*
-call buildkite-agent artifact download "fixtures\embedded\folder\*" "."
+call buildkite-agent artifact download "fixtures\embedded\folder\*" "d7"
 
 ECHO --- Downloading fixtures\embedded\**\*
-call buildkite-agent artifact download "fixtures\embedded\**\*" "."
+call buildkite-agent artifact download "fixtures\embedded\**\*" "d8"
 
 ECHO --- Downloading fixtures\**\*
-call buildkite-agent artifact download "fixtures\**\*" "."
+call buildkite-agent artifact download "fixtures\**\*" "d9"
 
 ECHO --- Downloading **\*
-call buildkite-agent artifact download "**\*" "."
+call buildkite-agent artifact download "**\*" "d10"
