4e8626311ec4ef4895b6b14308986bbef7f5483d
Keith Pitt <me@keithpitt.com>
2016-09-21 11:37:31 +1000

    Added spec for retryable jobs

--- tests/retryable-commands/fail-then-succeed.sh
+++ tests/retryable-commands/fail-then-succeed.sh
@@ -4,7 +4,10 @@ echo "The \$BUILDKITE_RETRY_COUNT value is $BUILDKITE_RETRY_COUNT"
 
 if [[ "$BUILDKITE_RETRY_COUNT" == "0" ]]; then
   echo "Gonna fail 👎"
-  exit 1
+  exit 123
+elif [[ "$BUILDKITE_RETRY_COUNT" == "1" ]]; then
+  echo "Gonna fail 👎 1 last time"
+  exit 456
 else
   echo "Horray! It works! 👍"
   exit 0
