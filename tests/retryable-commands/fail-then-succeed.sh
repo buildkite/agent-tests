41ffa7bda670a3a8637855b2cbe163301b90f48c
Keith Pitt <me@keithpitt.com>
2018-02-01 13:08:18 +0800

    Update fail-then-succeed.sh

--- tests/retryable-commands/fail-then-succeed.sh
+++ tests/retryable-commands/fail-then-succeed.sh
@@ -4,10 +4,10 @@ echo "The \$BUILDKITE_RETRY_COUNT value is $BUILDKITE_RETRY_COUNT"
 
 if [[ "$BUILDKITE_RETRY_COUNT" == "0" ]]; then
   echo "Gonna fail 👎"
-  exit 123
+  exit 1
 elif [[ "$BUILDKITE_RETRY_COUNT" == "1" ]]; then
   echo "Gonna fail 👎 1 last time"
-  exit 456
+  exit 2
 else
   echo "Horray! It works! 👍"
   exit 0
